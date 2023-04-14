0ce212a59a81f1028c3de0800463a41cb2b9d8d4
olga-evdokimova <pavedom13@gmail.com>
2023-04-14 21:49:30 +0300

    Update 2023-04-14T18:49:27.180Z

--- js/app.js
+++ js/app.js
@@ -3302,6 +3302,16 @@
     }
     window.addEventListener("resize", handleResize);
     handleResize();
+    const selectSingle = document.querySelector(".__select");
+    const selectSingle_title = selectSingle.querySelector(".__select__title");
+    const selectSingle_labels = selectSingle.querySelectorAll(".__select__label");
+    selectSingle_title.addEventListener("click", (() => {
+        if ("active" === selectSingle.getAttribute("data-state")) selectSingle.setAttribute("data-state", ""); else selectSingle.setAttribute("data-state", "active");
+    }));
+    for (let i = 0; i < selectSingle_labels.length; i++) selectSingle_labels[i].addEventListener("click", (evt => {
+        selectSingle_title.textContent = evt.target.textContent;
+        selectSingle.setAttribute("data-state", "");
+    }));
     window["FLS"] = true;
     isWebp();
     menuInit();
